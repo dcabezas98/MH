704de86d294400c3f3846e459433c0e9e43e2a6f
David Cabezas Berrido <dxabezas@gmail.com>
2021-03-11 23:42:21 +0100

    P1: greedy

---
+++ P1/software/fuentes/localSearch.cpp
@@ -0,0 +1,129 @@
+#include <iostream>
+#include <vector>
+#include <unordered_set>
+#include <stdlib.h>     // srand y rand
+#include <algorithm>    // advance
+#include <time.h>
+
+using namespace std;
+
+// Lee la entrada y la almacena en una matriz
+
+void readInput (vector<vector<double>> &mat) {
+  unsigned i, j;
+  double f;
+  int s = mat.size();
+  int iter = s*(s-1)/2;
+  for (int k=0; k<iter; k++) {
+      cin >> i >> j >> f;
+      mat[i][j] = mat[j][i] = f;
+  }
+}
+
+/********** Evaluación ***************/
+
+// Contribución de un elemento
+double singleContribution(unordered_set<int> &solution, vector<vector<double> > &mat, int elem) {
+  double result = 0;
+  unordered_set<int>::iterator it;
+  for (it = solution.begin(); it != solution.end(); it++) {
+    result += mat[ elem ][ *it ];
+  }
+  return result;
+}
+
+// Variedad de una solución
+double evaluateSolution(unordered_set<int> &solution, vector<vector<double> > &mat) {
+  double fitness = 0;
+  unordered_set<int>::iterator it;
+  for (it = solution.begin(); it != solution.end(); it++) {
+    fitness += singleContribution(solution, mat, *it);
+  }
+  return fitness /= 2; // Las cuenta doble
+}
+
+/******************* Local Search ********************/
+
+// Genera solución aleatoria
+unordered_set<int> randomSol(unsigned m, unsigned n){
+
+  int elem,rnd;
+
+  unordered_set<int> selected;
+  unordered_set<int> elements;
+  for (unsigned i=0; i<n; i++) {
+    elements.insert(i);
+  }
+
+  unordered_set<int>::iterator it;
+
+  while (selected.size()<m){ 
+    rnd=rand()%elements.size(); // Elijo un número aleatorio
+    it=elements.begin(); 
+    advance(it,rnd); // Selecciono el elemento correspondiente
+    elem=*it;
+    elements.erase( elem );
+    selected.insert( elem );
+  }
+
+  return selected;
+}
+
+int lowestContributor(unordered_set<int> &solution, vector<vector<double> > &mat, double &min_contrib){
+
+  int lowest;
+  double current_contrib;
+  unordered_set<int>::iterator it;
+
+  it = solution.begin();
+  lowest = *it;
+  min_contrib = singleContribution(solution, mat, lowest);
+  it++;
+  for ( ; it!=solution.end(); it++) {
+    current_contrib = singleContribution(solution, mat, *it); // Suma distancias del elemento a los seleccionados
+    if (current_contrib < min_contrib) { // Si la suma es menor, lo reemplaza
+      min_contrib = current_contrib;
+      lowest = *it;
+    }
+  }
+
+  return lowest;
+}
+
+void localSearch(vector<vector<double> > &mat, unsigned m) {
+
+  unordered_set<int> solution = randomSol(m,mat.size());
+  double variation=evaluateSolution(solution, mat);
+
+  int lowest;
+  double contrib, min_contrib;
+
+  bool carryon = true;
+  while (carryon){
+    carryon=false;
+    lowest=lowestContributor(solution, mat, min_contrib);
+    
+    // TODO: generar soluciones y probarlas
+    if (contrib > min_contrib){
+      variation = variation + contrib - min_contrib;
+      carryon=true;
+    }
+  }
+  cout << variation << endl;
+}
+
+/******************* MAIN **********************/
+
+int main( int argc, char *argv[] ) {
+  int n, m;
+
+  cin >> n >> m; // Leemos los parámetros n y m
+  vector<double> v (n, 0); // Vector de ceros con n componentes
+  vector<vector<double > > mat (n, v); // Matriz de nxn ceros
+  readInput(mat); // Leemos la entrada
+
+  cout << fixed;
+  srand(stoi(argv[1])); // SEED as parameter
+
+  localSearch(mat, m);
+}
---
+++ P1/software/run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Greedy
+
+#echo "" > salidas/greedy.txt
+
+#for filename in data/*.txt; do
+#	echo $filename >> salidas/greedy.txt
+#	bin/greedy < $filename >> salidas/greedy.txt
+#done
+
+# Local Search
+
+#for filename in data/*.txt; do
+#	echo $filename >> salidas/greedy.txt
+#	bin/greedy < $filename >> salidas/greedy.txt
+#done
